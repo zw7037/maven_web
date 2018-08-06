58e5a552dace638341227851f1a328f8b953b2b5
zw <15835206156@163.com>
2018-08-06 14:57:30 +0800

    meven_web

---
+++ src/main/java/com/rtdl/web/Wet.java
@@ -0,0 +1,10 @@
+package com.rtdl.web;
+
+/**
+ * Created by Administrator on 2018/8/6.
+ */
+public class Wet {
+    public static void main(String[] args) {
+        System.out.println("雨停了！");
+    }
+}
